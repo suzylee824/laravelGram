a9496acff7c3630816c6a423eb8abb16ea96e7c0
Suzy <suzylee824@gmail.com>
2020-09-06 10:46:30 -0700

    set up auth, profile page and routes, profile edit and routes

---
+++ app/Http/Controllers/ProfilesController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class ProfilesController extends Controller
+{
+    public function index(User $user)
+    {
+        $user = User::findOrFail($user);
+
+        return view('profiles.index', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
+    }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+
+        //$user->profile->update($data);
+
+        //return redirect("/profile/{$user->id}");
+
+        dd($data);
+    }
+}
---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+        <img alt="freecodecamp's profile picture" class="rounded-circle" src="https://scontent-lax3-1.cdninstagram.com/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=scontent-lax3-1.cdninstagram.com&amp;_nc_ohc=eTB_fA9eH0oAX_OxqKx&amp;oh=a1536b1bd5bedfc238b8c55a206773a7&amp;oe=5F7A38E7">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
+            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <a href="/p/{{ $post->id }}">
+                <img src="/storage/{{ $post->image }}" class="w-100">
+            </a>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
